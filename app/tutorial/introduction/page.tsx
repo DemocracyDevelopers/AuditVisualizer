e3a7b4ec01d729568c9a7d4ffdb7e0094ad17e39
lvyl9909 <141694918+lvyl9909@users.noreply.github.com>
2024-09-29 13:51:14 +1000

    feature: add tutorial pages

---
+++ app/tutorial/introduction/page.tsx
@@ -0,0 +1,10 @@
+// tutorial/introduction/page.tsx
+
+import React from "react";
+import IntroductionContent from "../components/IntroductionContent"; // Adjust the import path as needed
+
+const IntroductionPage: React.FC = () => {
+  return <IntroductionContent />;
+};
+
+export default IntroductionPage;
---
+++ app/tutorial/outcomes/page.tsx
@@ -0,0 +1,10 @@
+// tutorial/outcomes/page.tsx
+
+import React from "react";
+import OutcomesContent from "../components/OutcomesContent"; // Adjust the import path as needed
+
+const AssertionPage: React.FC = () => {
+  return <OutcomesContent />;
+};
+
+export default AssertionPage;
