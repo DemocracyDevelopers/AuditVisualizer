6542ef73046a93711c96d8b7d50021c786658cc0
xYoung0507 <xuyx12@student.unimelb.edu.au>
2024-10-03 18:45:57 +1000

    feat: add change color method

--- app/dashboard/components/Avatar.tsx
+++ app/dashboard/components/Avatar.tsx
@@ -1,19 +1,66 @@
 // dashboard/components/Avatar.tsx
 
-import React from "react";
+import React, { useEffect } from "react";
 import { AvatarColor } from "@/utils/avatarColor";
+import create from "zustand";
 
 interface AvatarProps {
   userId: number;
   userName: string;
   totalUsers: number;
 }
+interface Candidate {
+  id: number;
+  name: string;
+  color: string;
+}
+
+const useStore = create<{
+  candidateList: Candidate[];
+  assignColorsToCandidates: () => void;
+}>((set) => ({
+  candidateList: [
+    {
+      id: 0,
+      name: "Candidate 1",
+      color: "",
+    },
+    {
+      id: 1,
+      name: "Candidate 2",
+      color: "",
+    },
+  ],
+
+  assignColorsToCandidates: () => {
+    const avatarColor = new AvatarColor();
+    set((state) => ({
+      candidateList: state.candidateList.map((candidate) => ({
+        ...candidate,
+        color: avatarColor.getColor(candidate.id),
+      })),
+    }));
+  },
+}));
 
 const avatarColor = new AvatarColor();
 
 const Avatar: React.FC<AvatarProps> = ({ userId, userName, totalUsers }) => {
   const isWithinColorLimit = userId < 16; //
   const avatarSize = 40; // avatar size
+
+  const assignColorsToCandidates = useStore(
+    (state) => state.assignColorsToCandidates,
+  );
+  const candidateList = useStore((state) => state.candidateList);
+
+  useEffect(() => {
+    assignColorsToCandidates(); // 分配颜色给所有候选人
+    console.log(candidateList);
+  }, [assignColorsToCandidates, candidateList]);
+
+  const candidate = candidateList.find((candidate) => candidate.id === userId);
+  const backgroundColor = candidate ? candidate.color : "#CCCCCC";
 
   const styles: React.CSSProperties = {
     width: avatarSize,
@@ -24,9 +71,7 @@ const Avatar: React.FC<AvatarProps> = ({ userId, userName, totalUsers }) => {
     justifyContent: "center",
     color: "#FFFFFF",
     fontWeight: "bold",
-    backgroundColor: isWithinColorLimit
-      ? avatarColor.getColor(userId)
-      : "#CCCCCC", // 超出16时使用默认灰色
+    backgroundColor: backgroundColor, // 使用 Zustand 中的颜色
     fontSize: 14,
     textTransform: "uppercase",
   };
