1a0c920baaff2456c63fa55e1fec7927fb927c5d
zhangyuyuan <yuyuzhang1@student.unimelb.edu.au>
2025-04-02 14:51:32 +1100

    feat: add crown to the winner in the candidate list bar

--- app/dashboard/components/elimination-tree/candidate-list-bar.tsx
+++ app/dashboard/components/elimination-tree/candidate-list-bar.tsx
@@ -3,11 +3,13 @@ import {
   TooltipContent,
   TooltipProvider,
 } from "@/components/ui/tooltip";
+import { Crown } from "lucide-react";
 import { Candidate } from "./constants";
 import SearchDropdown from "./search-dropdown";
 import { TooltipTrigger } from "@radix-ui/react-tooltip";
 import { useState } from "react";
 import { useEffect, useRef } from "react";
+import useMultiWinnerDataStore from "@/store/multi-winner-data";
 
 type CandidateListBarProps = {
   selectedWinnerId: number | null;
@@ -22,6 +24,7 @@ function CandidateListBar({
   useAvatar,
   candidateList,
 }: CandidateListBarProps) {
+  const { winnerInfo } = useMultiWinnerDataStore();
   const scrollContainerRef = useRef<HTMLDivElement | null>(null); // Ref for the scroll container
   useEffect(() => {
     const el = scrollContainerRef.current;
@@ -43,7 +46,7 @@ function CandidateListBar({
   return (
     <div className="flex justify-center mb-5 gap-10">
       <div
-        className="flex overflow-x-auto w-64 border rounded-md px-2 gap-2"
+        className="flex gap-2 rounded-md w-[220px] px-2 overflow-x-scroll" // so the space can show the 4 candidates
         ref={scrollContainerRef}
       >
         {candidateList.map((candidate) => (
@@ -51,6 +54,13 @@ function CandidateListBar({
             key={candidate.id}
             className="flex flex-col items-center w-12 flex-shrink-0"
           >
+            <div className="h-5 mb-1 flex items-center justify-center">
+              {winnerInfo?.id === candidate.id ? (
+                <Crown className="text-yellow-500 h-5 w-5" />
+              ) : (
+                <div className="h-5 w-5 opacity-0" />
+              )}
+            </div>
             <TooltipProvider>
               <Tooltip>
                 {/*<TooltipTrigger*/}
