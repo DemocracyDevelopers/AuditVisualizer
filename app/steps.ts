30b192b21235d80375baa75965f02207210c3e90
lvyl9909 <141694918+lvyl9909@users.noreply.github.com>
2025-04-02 17:02:37 +1100

    feat:add tour guide function

--- app/steps.ts
+++ app/steps.ts
@@ -16,8 +16,22 @@ export const steps = [
     content: "This is the Elimination Tree showing candidate elimination path",
   },
   {
+    selector: "[data-tour='fifth-step']",
+    content:
+      "This bar shows all candidates. Click a name to highlight and view elimination path.",
+  },
+  {
     selector: "[data-tour='sixth-step']",
     content:
       "This is a step-by-step bar. Click each step to explore assertions individually.",
+  },
+  {
+    selector: "[data-tour='seventh-step']",
+    content: "This is our elimination tree!",
+  },
+  {
+    selector: "[data-tour='ninth-step']",
+    content:
+      "This section displays the currently applied assertion at the selected step. You can also revert it to view the tree before the assertion was applied.",
   },
 ];
