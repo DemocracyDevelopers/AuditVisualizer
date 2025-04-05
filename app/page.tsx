8d56b6a2a5aafd397797e40ed03f949e259233c4
mirrorgo <cloudmirrorgo@163.com>
2025-04-05 22:04:05 +1100

    feat: remove unused test components from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import TestComponent1 from "@/components/test-component-1";
-import TestComponents2 from "@/components/test-component-2";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -9,8 +7,6 @@ export default function Home() {
   return (
     <main>
       <div>test</div>
-      <TestComponent1 />
-      <TestComponents2 />
       <Link href="/new-page">
         <Button variant="link" className="hover:text-blue-600">
           click here: new page
