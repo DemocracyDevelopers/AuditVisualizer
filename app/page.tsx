2cadcc67f932029bcc48912d247b73c97b8ee7a7
mirrorgo <cloudmirrorgo@163.com>
2025-04-23 22:12:36 +1000

    refactor: Clean up Home and Dashboard components by removing unused imports and code

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default function Home() {
   return redirect("/upload");
-  return (
-    <main>
-      <div>test</div>
-      <Link href="/new-page">
-        <Button variant="link" className="hover:text-blue-600">
-          click here: new page
-        </Button>
-      </Link>
-    </main>
-  );
 }
