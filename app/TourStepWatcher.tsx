ec9c32a0408e571c47c3e4d8f7f5a0711d72be95
fuxingz <fuxingz@student.unimelb.edu.au>
2025-06-02 18:40:55 +1000

    delete comment in TourStepWatcher.tsx

--- app/TourStepWatcher.tsx
+++ app/TourStepWatcher.tsx
@@ -3,11 +3,8 @@ import { useEffect } from "react";
 import { useTour } from "@reactour/tour";
 import { expandTreeLayerByLayer } from "@/utils/treeActions";
 
-/** 映射每一步对应的副作用 */
 const stepEffects: Record<number, () => void> = {
   6: () => {
-    console.log("模拟Expand All");
-    // 可以触发标记已看、埋点、或者状态更新
     expandTreeLayerByLayer();
   },
 };
@@ -19,5 +16,5 @@ export const TourStepWatcher = () => {
     if (effect) effect();
   }, [currentStep]);
 
-  return null; // 不渲染任何 UI
+  return null; 
 };
